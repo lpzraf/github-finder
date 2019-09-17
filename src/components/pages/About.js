0b49926d4440480dc38aad55ed2e168e12d65245
Rafael Lopez <lpz.raf@gmail.com>
2019-09-16 21:56:45 -0400

    Adding the project to Github repo.

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About This App</h1>
+            <p>Web application made by Rafael Lopez with React JS.</p>
+            <p>Usecase: search engine for finding Github users.</p>
+            <p>Version: 1.0.0</p>
+        </Fragment>
+    )
+}
+
+export default About
